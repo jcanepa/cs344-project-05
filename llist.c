a49b9d7f0c62845fbef0c70c89d76b322ad3705f
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-06 11:41:05 -0800

    Stub functions defined in header file.

--- llist.c
+++ llist.c
@@ -1,7 +1,41 @@
 #include "llist.h"
 
+void llist_insert_head(struct node **head, struct node *n)
+{
+    return;
+}
+
+struct node *llist_delete_head(struct node **head)
+{
+    return;
+}
+
+void llist_insert_tail(struct node **head, struct node *n)
+{
+    return;
+}
+
+void llist_print(struct node *head)
+{
+    return;
+}
+
+void llist_free(struct node **head)
+{
+    return;
+}
+
+struct node *node_alloc(int value)
+{
+    return;
+}
+
+void node_free(struct node *n)
+{
+    return;
+}
+
 int main(int argc, char const *argv[])
 {
-    /* code */
     return 0;
 }
