843d162e838626d74fb0e786924b985502e21e1c
Julian Canepa <julian.l.canepa@gmail.com>
2023-02-11 18:13:18 -0800

    Implement logic for command parser the idiomatic C way.

--- llist.c
+++ llist.c
@@ -1,6 +1,7 @@
 #include "llist.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 void llist_insert_head(struct node **head, struct node *n)
 {
@@ -40,13 +41,37 @@ void node_free(struct node *n)
 int main(int argc, char const *argv[])
 {
     // command line parser
+    for (char **arg = argv + 1; *arg != argv[argc]; arg++)
+    {
+        if (strcmp(*arg, "ih") == 0)
+        {
+            printf("%s inserts %s at the head of the list\n", *arg, *(arg + 1));
+        }
+        else if (strcmp(*arg, "it") == 0)
+        {
+            printf("%s inserts %s to tail of the list\n", *arg, *(arg + 1));
+        }
+        else if (strcmp(*arg, "dh") == 0)
+        {
+            printf("%s deletes head node\n", *arg);
+        }
+        else if (strcmp(*arg, "f") == 0)
+        {
+            printf("%s frees the list\n", *arg);
+        }
+        else if (strcmp(*arg, "p") == 0)
+        {
+            printf("%s prints the list\n", *arg);
+        }
+    }
+
     // node_alloc()
     // node_free()
     // llist_print()
     // llist_insert_head()
     // ...
 
-    struct node *head;
+    // struct node *head;
 
     return 0;
 }
